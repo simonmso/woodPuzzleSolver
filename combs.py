d908a8b77dc7fd2ab722f5808b29a186fe569e2b
simonmso <56710751+simonmso@users.noreply.github.com>
2022-04-02 22:31:34 -0600

    works!

--- combs.py
+++ combs.py
@@ -1,5 +1,10 @@
 from prettyPrint import pp
 
+
+# ex.
+# abc, acb, cab, ...
+# optionsArr = ['a', 'b', 'c']
+# ret. [['a', 'b', 'c'], ['a', 'c', 'b'], ...]
 def getCombs (optionsArr, curComb = []):
     allCombs = []
     shortenedArray = [ *optionsArr ]
@@ -12,5 +17,22 @@ def getCombs (optionsArr, curComb = []):
             allCombs.extend(newCombs)
         else:
             allCombs.append(copy)
+
+    return allCombs
+
+# ex.
+# 000, 001, 002, 003, 010, 011, ...
+# digits = [0, 1, 2, 3]
+# places = 3
+def getAllCombs (digits, places, curComb = []):
+    allCombs = []
+    if len(curComb) == places:
+        allCombs.append(curComb)
+    else:
+        for digit in digits:
+            newComb = [ *curComb ]
+            newComb.append(digit)
+            newCombs = getAllCombs(digits, places, newComb)
+            allCombs += newCombs
 
     return allCombs
--- woodPuzzleSolver.py
+++ woodPuzzleSolver.py
@@ -1,33 +1,47 @@
-from pieces import pieces, printPiece
+from pieces import pieces, printPiece, printPieces
 from prettyPrint import pp
 from transaltor import translations
 from overlapChecker import piecesOverlap
-from combs import getCombs
+from combs import getCombs, getAllCombs
 from rotator import turnPieceNTimes
 
 def main ():
     positions = ['a', 'b', 'c', 'd', 'e', 'f']
-    allCombs = getCombs(positions)
+    allMoveCombs = getCombs(positions)
+    digits = [0, 1, 2, 3]
+    allRotCombs = getAllCombs(digits, 6)
 
-    for (i, comb) in enumerate(allCombs):
-        tmpPieces = []
-        for (pieceIdx, piece) in enumerate(pieces):
-            newPos = comb[pieceIdx]
-            movedPiece = translations[newPos](piece)
-            tmpPieces.append(movedPiece)
-        if not piecesOverlap(tmpPieces):
-            print('Success!', ''.join(comb))
-        if i % 100 == 0: print(i, 'tested')
+    for (i, moveComb) in enumerate(allMoveCombs):
+        for rotComb in allRotCombs:
+            # rotate pieces
+            rotPieces = []
+            for (pieceIdx, piece) in enumerate(pieces):
+                turns = rotComb[pieceIdx]
+                rotPiece = turnPieceNTimes(piece, turns)
+                rotPieces.append(rotPiece)
+            # move pieces
+            movedPieces = []
+            for (pieceIdx, piece) in enumerate(rotPieces):
+                newPos = moveComb[pieceIdx]
+                movedPiece = translations[newPos](piece)
+                movedPieces.append(movedPiece)
+            if not piecesOverlap(movedPieces):
+                print('Success!', ''.join(moveComb), ' ', rotComb)
+        if i % 25 == 0: print(i, 'tested')
 
 def tmp ():
-    piece = pieces[1]
-    for turns in range(4):
-        rot = turnPieceNTimes(piece, turns)
-        print(f'after {turns} turns:')
-        printPiece(rot)
+    printPieces(pieces)
+    
+
+    # testing rotation
+    # piece = pieces[1]
+    # for turns in range(4):
+    #     rot = turnPieceNTimes(piece, turns)
+    #     print(f'after {turns} turns:')
+    #     printPiece(rot)
 
 
-# if (__name__ == '__main__'): main()
-if (__name__ == '__main__'): tmp()
+if (__name__ == '__main__'): main()
+# if (__name__ == '__main__'): tmp()
 
 
